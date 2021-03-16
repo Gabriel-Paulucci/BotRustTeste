55c86e4f56afd34bb8c815c5ce0c8c1a4501cbf2
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2021-03-15 21:03:16 -0300

    Removendo token

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use event::Events;
 
 #[tokio::main]
 async fn main() -> Result<(), serenity::Error> {
-    let mut client = Client::builder("NzMwMDk0Mjg3MzQ1MTU2MTM2.XwSfSg.IsMQ6MggYhgrmfkLdYohdlzRHx0")
+    let mut client = Client::builder("")
         .framework(get_framework())
         .event_handler(Events)
         .await?;
